e4126c32419f0f930fa3a82a9541b34308879fcf
이길수 <igilsu@igilsuui-Macmini.local>
2022-11-23 19:47:41 +0900

    Add:roomForsocketJoin

--- server/index.js
+++ server/index.js
@@ -15,10 +15,24 @@ const io = new Server(server, {
   },
 });
 
+// io.on('connection', (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+//   socket.on('send_message', (data) => {
+//     socket.broadcast.emit('receive_message', data);
+//   });
+// });
+
 io.on('connection', (socket) => {
   console.log(`User Connected: ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    console.log('join한 방번호는 ' + data);
+    socket.join(data);
+  });
+
   socket.on('send_message', (data) => {
-    socket.broadcast.emit('receive_message', data);
+    console.log('메세지온 것이 ' + data.message + ', 메세지 보낸놈 방번호는  ' + data.room);
+    socket.to(data.room).emit('receive_message', data);
   });
 });
 
